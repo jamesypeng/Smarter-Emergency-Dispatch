cc205deaa3d89e58af514d478f1df5c1c64dd7f9
Priya Gupta <priyagupta@Priyas-MacBook-Pro.local>
2017-11-09 20:43:37 -0800

    test

--- test/test/map/admin.py
+++ test/test/map/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import EMS_Calls,Ambulance,Predictions
 # Register your models here.
+
+admin.site.register(EMS_Calls)
--- test/test/map/models.py
+++ test/test/map/models.py
@@ -2,8 +2,27 @@ from django.db import models
 
 # Create your models here.
 
-class Prob(models.Model):
-	prob_id = models.IntegerField()
-	zipcode = models.CharField(max_length=5)
-	predicted_ems = models.FloatField()
+class Predictions(models.Model):
+    zcta = models.CharField(max_length=5)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day_of_month = models.IntegerField()
+    hour_of_day = models.IntegerField()
+    day_of_year = models.IntegerField()
+    week_of_year = models.IntegerField()
+    day_of_week = models.IntegerField()
+    is_weekend = models.BooleanField()
+    Call_counts = models.FloatField()
 
+
+class Ambulance(models.Model):
+	amb_id = models.IntegerField()
+	LAT = models.FloatField()
+	LONG = models.FloatField()
+	AVAILABLE = models.IntegerField()
+
+
+class EMS_Calls(models.Model):
+	LAT = models.FloatField()
+	LONG = models.FloatField()
+	time = models.DateTimeField(auto_now_add=True)
