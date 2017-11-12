8fab432a40d683562f921c709aebe003d751d444
Melanie Costello <melaniecostello@Melanies-MacBook-Pro.local>
2017-11-11 16:59:44 -0800

    work on forms

--- test/test/map/admin.py
+++ test/test/map/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import EMS_Calls,Ambulance,Predictions
+from .models import *
 # Register your models here.
 
 admin.site.register(EMS_Calls)
+
+admin.site.register(Current_emscall)
--- test/test/map/urls.py
+++ test/test/map/urls.py
@@ -1,11 +1,18 @@
-from django.conf.urls import url
+# from . import views
+# from django.contrib import admin
+# from django.views.generic import TemplateView
+# from django.conf.urls import include, url
 
-from . import views
-from django.contrib import admin
-from django.views.generic import TemplateView
+# urlpatterns = [
+# url(r'^admin/', include(admin.site.urls)),
+# url(r'^$', TemplateView.as_view(template_name="map.html")),
+# url(r'', include('map.urls')),
+# ]
+
+
 from django.conf.urls import include, url
-urlpatterns = [
-url(r'^admin/', include(admin.site.urls)),
-url(r'^$', TemplateView.as_view(template_name="map.html")),
-]
+from . import views
 
+urlpatterns = [
+    url(r'^$', views.current_state, name='current_state'),
+]
