a78f364cd6a02d352d72958e04a1ab1de5042f35
James Peng <james.y.peng@gmail.com>
2017-11-18 21:43:36 -0800

    Add map page

--- Docker-Django/config/urls.py
+++ Docker-Django/config/urls.py
@@ -8,6 +8,7 @@ from django.views import defaults as default_views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^map/$', TemplateView.as_view(template_name='pages/map.html'), name='map'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
