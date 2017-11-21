3fe7cbc6ea2e8c86d82464423e32f8be48f61380
Priya Gupta <priyagupta@Priyas-MacBook-Pro.local>
2017-11-20 19:53:58 -0800

    current preds into model

--- test/test/map/models.py
+++ test/test/map/models.py
@@ -57,6 +57,26 @@ class Current_predictions(models.Model):
     def __str__(self):
         return self.zcta + "_" + str(self.call_counts)
 
+
+    def get_current_preds(self):
+       month = datetime.datetime.now().month
+       day = datetime.datetime.now().day
+       hour = datetime.datetime.now().hour
+       year = datetime.datetime.now().year
+       
+       #clear our current_predictions tables
+       Current_predictions.objects.all().delete()
+
+       #filter the predictions tables
+       t = Predictions.objects.filter(year=year,month=month,day_of_month=day,hour_of_day=hour).values_list()
+
+       #fill up current predictions table
+       for line in t:
+        t2 = Current_predictions(zcta=line[1], year=line[2], month=line[3],day_of_month=line[4],
+            hour_of_day=line[5], day_of_year=line[6],week_of_year=line[7],day_of_week=line[8],
+            is_weekend=line[9],call_counts=line[10])
+        t2.save()
+
 class Current_ambulance(models.Model):
     amb_id = models.IntegerField(primary_key=True)
     LAT = models.FloatField()
