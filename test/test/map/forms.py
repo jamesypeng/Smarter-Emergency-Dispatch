811bf365a95d48535e096997d67a05942a440b5e
Melanie Costello <melaniecostello@Melanies-MacBook-Pro.local>
2017-11-29 20:02:38 -0800

    Adding amb drop down

--- test/test/map/forms.py
+++ test/test/map/forms.py
@@ -30,13 +30,18 @@ class CurrentCallForm(forms.ModelForm):
 # 		fields = ("amb_id",)
 
 
-# USING THIS FORM RETURNS THE FOLLOWING ERROR: Current_ambulance matching query does not exist.
+def get_my_choices():
+    # you place some logic here
+	choices_list = Current_ambulance.objects.values_list('amb_id', flat=True)
+	return [(x, x) for x in choices_list]
+
+
+
 class CurrentAmbulanceForm(forms.Form):
 	"""Form to input id of ambulance that is now back in service"""
 	def __init__(self, *args, **kwargs):
         # first call parent's constructor
 		super(CurrentAmbulanceForm, self).__init__(*args, **kwargs)
         # there's a `fields` property now
+		self.fields['amb_id'] = forms.ChoiceField(choices=get_my_choices(), label="Ambulance ID" )
 		self.fields['amb_id'].required = False
-
-	amb_id = forms.IntegerField(label="Ambulance ID")
