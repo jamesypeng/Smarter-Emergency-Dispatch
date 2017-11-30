4bccbf146944523518307476812b4759b8291cb0
Priya Gupta <priyagupta@Priyas-MacBook-Pro.local>
2017-11-29 18:33:49 -0800

    update predictions

--- test/test/load_data.py
+++ test/test/load_data.py
@@ -6,7 +6,7 @@ from map.models import Current_ambulance, Predictions
 
 
 amb_file_path = "/Users/priyagupta/desktop/mids/w210/project/Smarter-Emergency-Dispatch/together/ambulance_loc2.csv"
-ems_preds_path = "/Users/priyagupta/desktop/mids/w210/project/Smarter-Emergency-Dispatch/together/ems_calls_predict_mod2.csv"
+ems_preds_path = "/Users/priyagupta/desktop/mids/w210/project/Smarter-Emergency-Dispatch/test/test/ems_calls_predict_2017-11-29.csv"
 
 reader_amb = csv.reader(open(amb_file_path))
 
@@ -20,6 +20,6 @@ for line in reader_amb:
 
 for line2 in reader_ems:
      t2 = Predictions(zcta=line2[0], year=line2[1], month=line2[2],day_of_month=line2[3],
-     	hour_of_day=line2[4], day_of_year=line2[5],week_of_year=line2[6],day_of_week=line2[7],
-     	is_weekend=line2[8],call_counts=line2[9])
+        hour_of_day=line2[4], day_of_year=line2[5],week_of_year=line2[6],day_of_week=line2[7],
+        is_weekend=line2[8].strip(''),call_counts=line2[9])
      t2.save()
