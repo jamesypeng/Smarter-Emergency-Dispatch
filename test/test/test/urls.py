5d61faf3b12f17d2ea5f5737325a77dfa71dc53b
James Peng <james.y.peng@gmail.com>
2017-12-04 07:05:38 +0800

    add sb_admin

--- test/test/test/urls.py
+++ test/test/test/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^django-sb-admin/', include('django_sb_admin.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'', include('map.urls')),
 ]
