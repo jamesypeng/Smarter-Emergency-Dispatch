8004237751a15bd58a8bc317eac5808d602fb79c
James Peng <james.y.peng@gmail.com>
2017-12-08 20:59:48 +0800

    add sb_admin_main

--- test/test/django_sb_admin/urls.py
+++ test/test/django_sb_admin/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 import django_sb_admin.views
 
 urlpatterns = [
-    url(r'^$', django_sb_admin.views.map, name='sb_admin_map'),
+    # url(r'^$', django_sb_admin.views.map, name='sb_admin_map'),
+    url(r'^$', django_sb_admin.views.main, name='sb_admin_main'),
     url(r'^dashboard/$', django_sb_admin.views.dashboard, name='sb_admin_dashboard'),
     url(r'^charts/$', django_sb_admin.views.charts, name='sb_admin_charts'),
-#	url(r'^map/$', include('map.urls'), name='map'),
     url(r'^tables/$', django_sb_admin.views.tables, name='sb_admin_tables'),
     url(r'^forms/$', django_sb_admin.views.forms, name='sb_admin_forms'),
     url(r'^bootstrap-elements/$', django_sb_admin.views.bootstrap_elements, name='sb_admin_bootstrap_elements'),
